bd58c201dffe18318ad2be596d4dcb579164517a
AanshiSachan <aanshi.work@gmail.com>
2020-05-03 23:23:37 +0530

    test commit

--- src/data/posts.js
+++ src/data/posts.js
@@ -8,7 +8,7 @@ const Posts =[{
     imageLink:"https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
 },{
     id:2,
-    description:'good pic',
+    description:'good picture',
     imageLink:"https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
     "3919321_1443393332_n.jpg"
 
